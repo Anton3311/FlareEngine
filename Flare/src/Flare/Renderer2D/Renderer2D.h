65c4153db267cbd5826770d643c17997ca0b69df
Anton3311 <antonplakhotia@gmail.com>
2023-06-10 16:08:52 +0300

    Batch rendering and OpenGL fixes

---
+++ Flare/src/Flare/Renderer2D/Renderer2D.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <Flare/Renderer/Buffer.h>
+#include <Flare/Renderer/VertexArray.h>
+#include <Flare/Renderer/Shader.h>
+
+#include <glm/glm.hpp>
+
+#include <vector>
+
+namespace Flare
+{
+	struct QuadVertex
+	{
+		glm::vec2 Position;
+		glm::vec4 Color;
+	};
+
+	struct Renderer2DData
+	{
+		size_t QuadIndex;
+		size_t MaxQuadCount;
+
+		std::vector<QuadVertex> Vertices;
+
+		Ref<VertexArray> VertexArray;
+		Ref<VertexBuffer> VertexBuffer;
+		Ref<IndexBuffer> IndexBuffer;
+
+		Ref<Shader> CurrentShader;
+	};
+
+	class Renderer2D
+	{
+	public:
+		static void Initialize(size_t maxQuads = 10000);
+		static void Shutdown();
+
+		static void Begin(const Ref<Shader>& shader);
+		static void Flush();
+		static void Submit(glm::vec2 position, glm::vec2 size, glm::vec4 color);
+		static void End();
+	private:
+		static Renderer2DData* s_Data;
+	};
+}
