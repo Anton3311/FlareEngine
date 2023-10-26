5b65667fa5e8414a95c59c1e2d92d7ce73ff3538
Anton3311 <antonplakhotia@gmail.com>
2023-10-26 17:41:25 +0300

    Materials refactoring

--- Flare/src/Flare/Renderer/Material.h
+++ Flare/src/Flare/Renderer/Material.h
@@ -1,18 +1,21 @@
 #pragma once
 
 #include "Flare/AssetManager/Asset.h"
+#include "Flare/Renderer/Shader.h"
 
 namespace Flare
 {
 	class FLARE_API Material : public Asset
 	{
 	public:
+		Material(Ref<Shader> shader);
 		Material(AssetHandle shaderHandle);
 		virtual ~Material();
 
-		inline AssetHandle GetShaderHandle() const { return m_ShaderHandle; }
+		inline AssetHandle GetShaderHandle() const { return m_Shader->Handle; }
+		inline Ref<Shader> GetShader() const { return m_Shader; }
 
-		void SetInt(uint32_t index, int32_t value);
+		/*void SetInt(uint32_t index, int32_t value);
 		void SetInt2(uint32_t index, glm::ivec2 value);
 		void SetInt3(uint32_t index, const glm::ivec3& value);
 		void SetInt4(uint32_t index, const glm::ivec4& value);
@@ -21,25 +24,37 @@ namespace Flare
 		void SetFloat2(uint32_t index, glm::vec2 value);
 		void SetFloat3(uint32_t index, const glm::vec3& value);
 		void SetFloat4(uint32_t index, const glm::vec4& value);
+		void SetMatrix4(uint32_t index, const glm::mat4& matrix);*/
 		void SetIntArray(uint32_t index, const int32_t* values, uint32_t count);
-		void SetMatrix4(uint32_t index, const glm::mat4& matrix);
 
 		template<typename T>
 		T ReadParameterValue(uint32_t index)
 		{
-			FLARE_CORE_ASSERT((size_t)index < m_ShaderParametersCount);
+			const ShaderParameters& parameters = m_Shader->GetParameters();
+			FLARE_CORE_ASSERT((size_t)index < parameters.size());
+			FLARE_CORE_ASSERT(sizeof(T) == parameters[index].Size);
 
 			T value;
 
-			const ShaderParameters& parameters = AssetManager::GetAsset<Shader>(m_ShaderHandle)->GetParameters();
 			memcpy_s(&value, sizeof(value), m_Buffer + parameters[index].Offset, parameters[index].Size);
-
 			return value;
+		}
+
+		template<typename T>
+		void WriteParameterValue(uint32_t index, const T& value)
+		{
+			const ShaderParameters& parameters = m_Shader->GetParameters();
+			FLARE_CORE_ASSERT((size_t)index < parameters.size());
+			FLARE_CORE_ASSERT(sizeof(T) == parameters[index].Size);
+
+			memcpy_s(m_Buffer + parameters[index].Offset, sizeof(value), &value, parameters[index].Size);
 		}
 
 		void SetShaderParameters();
 	private:
-		AssetHandle m_ShaderHandle;
+		void Initialize();
+	private:
+		Ref<Shader> m_Shader;
 
 		size_t m_ShaderParametersCount;
 		size_t m_BufferSize;
