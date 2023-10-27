121428008ffeb4119032a1331fcf61d0aa4bfa62
Anton3311 <antonplakhotia@gmail.com>
2023-10-27 09:10:19 +0300

    Fixed spirv cache reading in Release build

--- Flare/src/Flare/Platform/OpenGL/OpenGLShader.cpp
+++ Flare/src/Flare/Platform/OpenGL/OpenGLShader.cpp
@@ -231,6 +231,8 @@ namespace Flare
         size_t size = inputStream.tellg();
         inputStream.seekg(0, std::ios::beg);
 
+        FLARE_CORE_INFO("Reading SPIRV: Size = {}", size);
+
         output.resize(size / sizeof(uint32_t));
         inputStream.read((char*)output.data(), size);
 
@@ -537,7 +539,8 @@ namespace Flare
             std::filesystem::path cachePath = cacheDirectory / Shader::GetCacheFileName(path.filename().string(), "vulkan", stageName);
             if (std::filesystem::exists(cachePath))
             {
-                FLARE_CORE_ASSERT(ReadSpirvData(cachePath, compiledShaderData));
+                bool result = ReadSpirvData(cachePath, compiledShaderData);
+                FLARE_CORE_ASSERT(result);
             }
             else
             {
@@ -572,7 +575,8 @@ namespace Flare
             cachePath = cacheDirectory / Shader::GetCacheFileName(path.filename().string(), "opengl", stageName);
             if (std::filesystem::exists(cachePath))
             {
-                FLARE_CORE_ASSERT(ReadSpirvData(cachePath, compiledShaderData));
+                bool result = ReadSpirvData(cachePath, compiledShaderData);
+                FLARE_CORE_ASSERT(result);
             }
             else
             {
