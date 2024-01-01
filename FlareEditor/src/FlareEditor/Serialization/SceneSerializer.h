40a2c522720ad6c0b544e3a2733f47bcde4c99d5
Anton3311 <antonplakhotia@gmail.com>
2024-01-01 13:39:09 +0200

    SceneSerialization: Serialization of entity ids

--- FlareEditor/src/FlareEditor/Serialization/SceneSerializer.h
+++ FlareEditor/src/FlareEditor/Serialization/SceneSerializer.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include "FlareCore/UUID.h"
+
 #include "Flare/Scene/Scene.h"
+#include "FlareECS/Entity/Entity.h"
 
 #include <yaml-cpp/yaml.h>
 
 #include <filesystem>
+#include <unordered_map>
 
 namespace Flare
 {
@@ -14,7 +18,7 @@ namespace Flare
 		static void SerializeComponent(YAML::Emitter& emitter, const World& world, Entity entity, ComponentId component);
 
 		static void SerializeEntity(YAML::Emitter& emitter, World& world, Entity entity);
-		static Entity DeserializeEntity(const YAML::Node& node, World& world);
+		static void DeserializeEntity(Entity entity, const YAML::Node& node, World& world, std::unordered_map<UUID, Entity>& serializationIdToECSId);
 
 		static void Serialize(const Ref<Scene>& scene);
 		static void Serialize(const Ref<Scene>& scene, const std::filesystem::path& path);
