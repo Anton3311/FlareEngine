5aa8aeddd7df53dc6ce443aade87aa5b61373e7a
Anton3311 <antonplakhotia@gmail.com>
2024-01-29 10:17:21 +0200

    Added R11G11B10 frame buffer format

--- Flare/src/Flare/Renderer/FrameBuffer.h
+++ Flare/src/Flare/Renderer/FrameBuffer.h
@@ -11,6 +11,7 @@ namespace Flare
 	{
 		RGB8,
 		RGBA8,
+		R11G11B10,
 
 		RedInteger,
 		RF32,
@@ -26,6 +27,7 @@ namespace Flare
 		{
 		case FrameBufferTextureFormat::RGB8:
 		case FrameBufferTextureFormat::RGBA8:
+		case FrameBufferTextureFormat::R11G11B10:
 		case FrameBufferTextureFormat::RedInteger:
 			return false;
 		case FrameBufferTextureFormat::Depth24Stencil8:
