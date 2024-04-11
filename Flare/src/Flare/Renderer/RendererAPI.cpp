f530c55438e5450178f2b18ef2a46de194d55f3f
Anton3311 <antonplakhotia@gmail.com>
2024-04-11 19:18:39 +0300

    Allow a rendering API to be specified as command line arg

--- Flare/src/Flare/Renderer/RendererAPI.cpp
+++ Flare/src/Flare/Renderer/RendererAPI.cpp
@@ -7,20 +7,33 @@
 namespace Flare
 {
 	RendererAPI::API s_API = RendererAPI::API::Vulkan;
+	Scope<RendererAPI> s_Instance = nullptr;
 
-	Scope<RendererAPI> RendererAPI::Create()
+	void RendererAPI::Create(RendererAPI::API api)
 	{
+		s_API = api;
+
 		switch (s_API)
 		{
 		case API::OpenGL:
-			return CreateScope<OpenGLRendererAPI>();
+			s_Instance.reset(new OpenGLRendererAPI());
+			break;
 		case API::Vulkan:
-			return CreateScope<VulkanRendererAPI>();
+			s_Instance.reset(new VulkanRendererAPI());
+			break;
 		default:
 			FLARE_CORE_ASSERT(false, "Unsupported rendering API");
 		}
+	}
 
-		return nullptr;
+	void RendererAPI::Release()
+	{
+		s_Instance.reset(nullptr);
+	}
+
+	const Scope<RendererAPI>& RendererAPI::GetInstance()
+	{
+		return s_Instance;
 	}
 
 	RendererAPI::API RendererAPI::GetAPI()
