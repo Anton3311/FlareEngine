d180c584aa57c8a25bb513a9dbbd889807ab2023
Anton3311 <antonplakhotia@gmail.com>
2024-07-13 19:41:14 +0300

    ECS: Refactored archetypes creation

--- FlareECS/src/FlareECS/ECSContext.h
+++ FlareECS/src/FlareECS/ECSContext.h
@@ -5,8 +5,10 @@
 
 namespace Flare
 {
-	struct ECSContext
+	struct FLAREECS_API ECSContext
 	{
+		ECSContext();
+
 		inline void Clear()
 		{
 			Archetypes.Clear();
--- FlareECS/src/FlareECS/Entity/Archetypes.h
+++ FlareECS/src/FlareECS/Entity/Archetypes.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "FlareCore/Assert.h"
+#include "FlareCore/Collections/Span.h"
 
 #include "FlareECS/Entity/Archetype.h"
 
@@ -9,9 +10,10 @@
 
 namespace Flare
 {
+	struct Components;
 	struct FLAREECS_API Archetypes
 	{
-		Archetypes() = default;
+		Archetypes(const Components& componentsRegistry);
 		~Archetypes();
 
 		Archetypes(const Archetypes&) = delete;
@@ -36,6 +38,12 @@ namespace Flare
 			return Records[(size_t)id];
 		}
 
+		const ArchetypeRecord* FindArchetype(Span<ComponentId> components) const;
+		const ArchetypeRecord* FindOrCreateArchetype(Span<ComponentId> components);
+		
+		ArchetypeId CreateArchetype(Span<const ComponentId> sortedComponentIds);
+		ArchetypeId CreateArchetype(std::vector<ComponentId>&& sortedComponentIds);
+
 		std::optional<size_t> GetArchetypeComponentIndex(ArchetypeId archetype, ComponentId component) const
 		{
 			FLARE_CORE_ASSERT(IsIdValid(archetype));
@@ -54,9 +62,11 @@ namespace Flare
 			else
 				return {};
 		}
-
+		
 		std::vector<ArchetypeRecord> Records;
 		std::unordered_map<ComponentSet, ArchetypeId> ComponentSetToArchetype;
 		std::unordered_map<ComponentId, std::unordered_map<ArchetypeId, size_t>> ComponentToArchetype;
+	private:
+		const Components& m_ComponentsRegistry;
 	};
 }
