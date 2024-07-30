7fea049d52ace6d394e327718fb3dfe731efe551
Anton3311 <antonplakhotia@gmail.com>
2024-07-30 10:15:30 +0300

    PrefabEditor: Set default directional & environment light

--- FlareEditor/src/FlareEditor/UI/PrefabEditor.cpp
+++ FlareEditor/src/FlareEditor/UI/PrefabEditor.cpp
@@ -18,6 +18,9 @@ namespace Flare
     {
         FLARE_PROFILE_FUNCTION();
         m_SceneRenderer.reset(new SceneRenderer(m_PreviewScene));
+        m_SceneRenderer->SetDefaultDirectionalLight(glm::normalize(glm::vec3(1.0f, -1.0f, 1.0f)), glm::vec3(1.0f), 2.0f);
+        m_SceneRenderer->SetDefaultEnvironmentLight(glm::vec3(1.0f), 0.8f);
+
         m_ViewportWindow.SetScene(m_PreviewScene);
 
         EditorCameraSettings& settings = m_EditorCamera.GetSettings();
