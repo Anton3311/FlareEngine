9591f0721dc0900f3e0418c19e095db9b5486bc5
Anton3311 <antonplakhotia@gmail.com>
2024-07-31 23:54:50 +0300

    ECS: Removed FilteredComponentsGroup

--- FlareECS/src/FlareECS/Entity/ComponentGroup.h
+++ FlareECS/src/FlareECS/Entity/ComponentGroup.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "FlareECS/Entity/Component.h"
-#include "FlareECS/Query/QueryFilters.h"
 
 #include <array>
 
@@ -24,27 +23,5 @@ namespace Flare
 		constexpr const std::array<ComponentId, sizeof...(Components)>& GetIds() const { return m_Ids; }
 	private:
 		std::array<ComponentId, sizeof...(Components)> m_Ids;
-	};
-
-	template<typename... T>
-	class FilteredComponentsGroup
-	{
-	public:
-		using ComponentsArray = std::array<ComponentId, sizeof...(T)>;
-
-		FilteredComponentsGroup()
-		{
-			size_t index = 0;
-
-			([&]
-			{
-				T filter;
-				m_Components[index++] = filter.Component;
-			} (), ...);
-		}
-	public:
-		constexpr const ComponentsArray& GetComponents() const { return m_Components; }
-	private:
-		ComponentsArray m_Components;
 	};
 }
