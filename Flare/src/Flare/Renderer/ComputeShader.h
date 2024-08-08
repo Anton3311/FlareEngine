9c78394d1b32556f6ecda86f7261848bf3e884a7
Anton3311 <antonplakhotia@gmail.com>
2024-08-08 11:34:18 +0300

    Derive ComputeShaderMetadata from ShaderMetadata

--- Flare/src/Flare/Renderer/ComputeShader.h
+++ Flare/src/Flare/Renderer/ComputeShader.h
@@ -9,12 +9,9 @@
 
 namespace Flare
 {
-	struct ComputeShaderMetadata
+	struct ComputeShaderMetadata : public ShaderMetadata
 	{
-		std::string Name;
 		glm::uvec3 LocalGroupSize = glm::uvec3(1u);
-		ShaderPushConstantsRange PushConstantsRange;
-		std::vector<ShaderProperty> Properties;
 	};
 
 	class FLARE_API ComputeShader : public Asset
