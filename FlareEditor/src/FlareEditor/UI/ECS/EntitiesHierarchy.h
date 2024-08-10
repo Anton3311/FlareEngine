bd384ae26de02f7874e5522e76c05a69b93b774d
Anton3311 <antonplakhotia@gmail.com>
2024-08-10 13:09:35 +0300

    EntitiesHierarchy refactor

--- FlareEditor/src/FlareEditor/UI/ECS/EntitiesHierarchy.h
+++ FlareEditor/src/FlareEditor/UI/ECS/EntitiesHierarchy.h
@@ -28,6 +28,10 @@ namespace Flare
 
 		inline void SetWorld(World& world) { m_World = &world; }
 	private:
+		bool RenderContextMenu(Entity& selectedEntity);
+		
+		Entity DuplicateEntity(Entity entity);
+	private:
 		bool RenderEntityItem(Entity entity, Entity& selectedEntity);
 		bool RenderEntityContextMenu(Entity entity, Entity& selectedEntity);
 	private:
