499e79c07cccbfd05dcd3cf071bb3b8f1b5c4d1d
Anton3311 <antonplakhotia@gmail.com>
2024-10-13 21:25:33 +0300

    Core: Added more common includes to PCHCommon.h

--- FlareCore/src/FlareCore/PCHCommon.h
+++ FlareCore/src/FlareCore/PCHCommon.h
@@ -2,11 +2,21 @@
 
 #include "FlareCore/Profiler/Profiler.h"
 
+#include "FlareCore/Assert.h"
+#include "FlareCore/Log.h"
+#include "FlareCore/Ref.h"
+#include "FlareCore/Core.h"
+
+#include "FlareCore/Collections/Span.h"
+
 #include <array>
+#include <cstring>
 #include <filesystem>
 #include <map>
 #include <set>
 #include <stdint.h>
+#include <string>
+#include <string_view>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
