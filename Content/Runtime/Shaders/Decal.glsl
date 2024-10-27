be3daa3643bd1f5ee3f7a04800d8de89a53d3fdb
Anton3311 <antonplakhotia@gmail.com>
2024-10-27 10:38:35 +0200

    Fixed Decal.glsl using descriptor set 2 instaed of set 1 for the depth texture

--- Content/Runtime/Shaders/Decal.glsl
+++ Content/Runtime/Shaders/Decal.glsl
@@ -33,7 +33,7 @@ void main()
 
 #include "Common/Camera.glsl"
 
-layout(set = 2, binding = 0) uniform sampler2D u_DepthTexture;
+layout(set = 1, binding = 0) uniform sampler2D u_DepthTexture;
 layout(set = 3, binding = 0) uniform sampler2D u_ColorTexture;
 
 layout(location = 0) in flat mat4 i_ViewToObjectSpace;
