d58cf59c6a3fb23b0ececa8f06328f5f234fa3a2
Anton3311 <antonplakhotia@gmail.com>
2024-11-01 20:53:09 +0200

    Added Emission property to Surface.glsl

--- Content/Runtime/Shaders/Surface.glsl
+++ Content/Runtime/Shaders/Surface.glsl
@@ -4,6 +4,7 @@ Properties =
 	u_Material.Color = { Type = Color }
 	u_Material.Roughness = {}
 	u_Material.Metallic = {}
+	u_Material.Emission = { Type = HDR }
 	u_Texture = { Default = White }
 	u_NormalMap = { Default = DefaultNormals }
 	u_RoughnessMap = { Default = White }
@@ -62,6 +63,7 @@ layout(std140, push_constant) uniform InstanceData
 	vec4 Color;
 	float Roughness;
 	float Metallic;
+	vec3 Emission;
 } u_Material;
 
 struct VertexData
@@ -142,6 +144,8 @@ void main()
 	finalColor += CalculateSpotLightsContribution(V, surface);
 
 	finalColor += u_EnvironmentLight.rgb * u_EnvironmentLight.w * color.rgb;
+
+	finalColor += u_Material.Emission;
 
 #if DEBUG_CASCADES
 	int cascadeIndex = CalculateCascadeIndex(i_Vertex.ViewSpacePosition);
