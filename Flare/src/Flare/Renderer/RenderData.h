a3ebdcaea353e1e474dfc90d7aafb51dad14ad61
Anton3311 <antonplakhotia@gmail.com>
2024-11-23 11:55:06 +0200

    Fixed ambient light not being applied inside the shadows when AO if off

--- Flare/src/Flare/Renderer/RenderData.h
+++ Flare/src/Flare/Renderer/RenderData.h
@@ -71,6 +71,18 @@ namespace Flare
 		uint32_t PointLightsCount = 0;
 		uint32_t SpotLightsCount = 0;
 
-		bool AOEnabled = false;
+		// Making it a `bool` type, causes errors when the `LightData` is copied to the GPU.
+		// spirv-cross says that this member of an equivalent struct inside the shader is actually a uint.
+		//
+		//    struct LightData {
+		//        float4 u_LightColor;       // abs offset =  0, rel offset =  0, size = 16, padded size = 16
+		//        float3 u_LightDirection;   // abs offset = 16, rel offset = 16, size = 12, padded size = 12
+		//        float  u_LightNear;        // abs offset = 28, rel offset = 28, size =  4, padded size =  4
+		//        float4 u_EnvironmentLight; // abs offset = 32, rel offset = 32, size = 16, padded size = 16
+		//        uint   u_PointLightsCount; // abs offset = 48, rel offset = 48, size =  4, padded size =  4
+		//        uint   u_SpotLightsCount;  // abs offset = 52, rel offset = 52, size =  4, padded size =  4
+		//        uint   u_AOEnabled;        // abs offset = 56, rel offset = 56, size =  4, padded size =  8
+		//    } ;
+		uint32_t AOEnabled = false;
 	};
 }
